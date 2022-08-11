66111dbd043111449d093e5ee8abf1af5dc96256
sebassys <sebassys@gmail.com>
2022-08-11 00:48:38 -0600

    REST Json in html

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -26,26 +26,13 @@ function procesarDatos() {
     let menuContainer = document.getElementById("menu-container-id");
     let html = "";
     datos.productos.forEach(producto => {
-        console.log(producto.nombre)
-        html += "<div class='col'>";
-        html += "<div class='card card-cover h-100 overflow-hidden text-bg-dark rounded-10 shadow-lg' style=\"background-image: url('" + producto.imagen +"');\">";
-        html += "<div class='d-flex flex-column h-100 p-3 pb-1 text-white text-shadow-1'>";
-        html += "<h2 class='display-6 lh-1 fw-bold'>" + producto.nombre + "</h2>";
-        html += "<p>" + producto.ingredientes + "</p>";
-        html += "<ul class='d-flex list-unstyled mt-auto'>";
-        html += "<li class='me-auto'>";
-        html += "<img src='" + producto.imagen + "' alt='Platillo' width='32' height='32' class='rounded-circle border border-white' />";
-        html += "</li>";
-        html += "<li class='d-flex align-items-center me-3'>";
-        html += "<svg class='bi me-2' width='1em' height='1em'><use xlink:href='#geo-fill' /></svg>";
-        html += "<small>" + producto.clasificacion + "</small>";
-        html += "</li>";
-        html += "<li class='d-flex align-items-center'>";
-        html += "<svg class='bi me-2' width='1em' height='1em'><use xlink:href='#calendar3' /></svg>";
-        html += "<small>" + producto.precio + "</small>";
-        html += "</li></ul></div></div></div>";
+        html += "<div class='col-lg-6 menu-item filter-specialty'>";
+        html += "<div class='menu-content'>";
+        html += "<a href='#'>" + producto.nombre + "</a><span>$" + producto.precio + "</span>";
+        html += "</div>";
+        html += "<div class='menu-ingredients'>" + producto.ingredientes + "</div>";
+        html += "</div>";
     });
     menuContainer.innerHTML= "";
     menuContainer.innerHTML = html;
-    console.log(html);
 }
